780e7c942486c3566ca2ea115bce282217a0087c
Obenson Maurice <37198381+obens@users.noreply.github.com>
2025-04-07 23:25:32 -0300

    Update DataModel.cs

--- SimpleApi/Models/DataModel.cs
+++ SimpleApi/Models/DataModel.cs
@@ -1,7 +1,12 @@
+// Define the namespace for the model class
 namespace SimpleApi.Models;
 
+// Define a simple data model class
 public class DataModel
 {
+    // Unique identifier for the data item
     public int Id { get; set; }
+
+    // Name or label of the data item
     public string Name { get; set; }
 }
